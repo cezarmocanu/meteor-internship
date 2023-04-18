df8aaf0ecf8de8ac6ece853a7cb6becafe2c36ec
cezarmocanu <cezarmocanu2014@gmail.com>
2023-04-18 20:32:23 +0300

    [432-7f5] Added auth navigation

---
+++ src/constants/route-paths.js
@@ -0,0 +1,13 @@
+const RoutePaths = {
+	ROOT: "/",
+	TEST: "/test",
+	LOGIN: "/login",
+	SIGNUP: "/signup",
+	FORGOT_PASSWORD: "/forgot-password",
+	CHANGE_PASSWORD: "/change-password",
+	WORKSPACE: "/workspace",
+	AUTH: "/auth",
+	PRIVATE: "/private",
+};
+
+export default RoutePaths;
