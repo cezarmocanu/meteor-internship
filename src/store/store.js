cdc432d7bc07a989b3ad21727f251ca6b043eec0
cezarmocanu <cezarmocanu2014@gmail.com>
2023-04-18 21:19:13 +0300

    [186-f32] Added slice to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CounterReducer from "./slices/counter-slice";
 import AuthenticationReducer from "./slices/authentication-slice";
+import ModalReducer from "./slices/modal-slice";
 
 const store = configureStore({
 	reducer: {
 		counter: CounterReducer,
 		authentication: AuthenticationReducer,
+		modal: ModalReducer,
 	},
 });
 
