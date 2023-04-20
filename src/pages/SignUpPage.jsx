40f7487b8a439c1c92059cd9643ce98ed73d8338
AlexandruAlin55 <alexandrualin21@gmail.com>
2023-04-20 20:47:54 +0300

    [168-cf7-Change-Label] Am modificat Label [168-cf7-Change-Label] Am adaugat Last Name

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -65,13 +65,22 @@ function SignUpPage() {
 								<Typography>It’s Simpe and Easy!!</Typography>
 							</Stack>
 							<Stack>
-								<FormLabel>Label</FormLabel>
+								<FormLabel>Full Name</FormLabel>
 								<TextField
 									required
 									variant="outlined"
-									helperText="Information about the label"
 									size="small"
+								
 								/>
+								
+								<FormLabel>Last Name</FormLabel>
+								<TextField
+									required
+									variant="outlined"
+									size="small"
+							
+								/>
+
 								<FormLabel>Email Address</FormLabel>
 								<TextField
 									required
