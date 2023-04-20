d4f2825412484f10b96ce37bf99f2422ddc94ab4
AleBarboni <alexa_wera@yahoo.com>
2023-04-20 11:22:42 +0300

    [108-cf7]

---
+++ src/components/modal-content/CreateTask.jsx
@@ -0,0 +1,33 @@
+import { useDispatch } from "react-redux";
+import { closeModal } from "../../store/slices/modal-slice";
+import {
+	DialogTitle,
+	DialogContent,
+	DialogContentText,
+	DialogActions,
+	Button,
+} from "@mui/material";
+
+function CreateTask() {
+	const dispatch = useDispatch();
+
+	return (
+		<>
+			<DialogActions>
+				<Button onClick={() => dispatch(closeModal())}>X</Button>
+			</DialogActions>
+			<DialogTitle>Create Task</DialogTitle>
+			<DialogContent>
+				<DialogContentText>
+					Let Google help apps determine location. This means sending anonymous
+					location data to Google, even when no apps are running.
+				</DialogContentText>
+			</DialogContent>
+			{/* <DialogActions>
+				<Button onClick={() => dispatch(closeModal())}>Create Task</Button>
+			</DialogActions> */}
+		</>
+	);
+}
+
+export default CreateTask;
