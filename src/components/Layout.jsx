321328c21ad0cdc1ff96dbb602f5995468348ae8
cezarmocanu <cezarmocanu2014@gmail.com>
2023-04-20 19:49:50 +0300

    [020-dae] Cleaned SideBar and added permanent variant

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,46 +1,13 @@
 import React from "react";
-import { styled } from "@mui/material/styles";
 import SideBar from "./SideBar";
-import { useState } from "react";
-import MuiAppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
+import { Stack } from "@mui/material";
 
-import Typography from "@mui/material/Typography";
-import MenuIcon from "@mui/icons-material/Menu";
-
-const AppBar = styled(MuiAppBar, {
-	shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({}));
-
-const drawerWidth = 240;
 const Layout = ({ children }) => {
-	const [isOpen, setIsOpen] = useState(false);
-
-	const toggleDrawer = () => {
-		setIsOpen((old) => !old);
-	};
 	return (
-		<div>
-			<AppBar position="fixed" open={isOpen}>
-				<Toolbar>
-					<IconButton
-						color="inherit"
-						aria-label="open drawer"
-						onClick={toggleDrawer}
-						edge="start"
-						sx={{ mr: 2, ...(isOpen && { display: "none" }) }}
-					>
-						<MenuIcon />
-					</IconButton>
-					<Typography variant="h6" noWrap component="div">
-						Persistent drawer
-					</Typography>
-				</Toolbar>
-			</AppBar>
-			<SideBar isOpen={isOpen} toggleDrawer={toggleDrawer} />
-			{children}
-		</div>
+		<Stack direction="row">
+			<SideBar />
+			<Stack flexGrow={1}>{children}</Stack>
+		</Stack>
 	);
 };
 
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,89 +1,62 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import Button from "@mui/material/Button";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
 import { useState } from "react";
-import Stack from "@mui/material/Stack";
-import AssignmentIcon from "@mui/icons-material/Assignment";
-import SettingsIcon from "@mui/icons-material/Settings";
-import AppsIcon from "@mui/icons-material/Apps";
-import { Typography } from "@mui/material";
-import Avatar from "@mui/material/Avatar";
+import { Drawer, Button, Typography, Stack, Avatar } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import Theme from "../theme";
+import SideBarItems from "./SideBarItems";
 
-export default function SideBar({ isOpen, toggleDrawer }) {
-	const menuItems = [
-		{
-			text: "Overview",
-			icon: AppsIcon,
-		},
-		{
-			text: "Tasks",
-			icon: AssignmentIcon,
-		},
-		{
-			text: "Settings",
-			icon: SettingsIcon,
-		},
-	];
-	const list = () => (
-		<Box
-			sx={{ auto: 300 }}
-			role="presentation"
-			onClick={toggleDrawer}
-			onKeyDown={toggleDrawer}
-			paddingRight={8}
-		>
-			<List>
-				{menuItems.map((item) => (
-					<ListItem key={item.text} disablePadding>
-						<ListItemButton>
-							<ListItemIcon>
-								<item.icon />
-							</ListItemIcon>
-							<ListItemText primary={item.text} />
-						</ListItemButton>
-					</ListItem>
-				))}
-			</List>
-		</Box>
-	);
+const BASE_DRAWER_WIDTH = 72;
+const EXPANDED_DRAWER_WIDTH = 408;
+
+function SideBar() {
+	const [isExpanded, setIsExpanded] = useState(false);
 
 	return (
-		<div>
-			<>
-				<Drawer anchor={"left"} open={isOpen} onClose={toggleDrawer}>
-					<Stack direction={"row"} height="100%">
-						<Stack
-							direction="column"
-							spacing={2}
-							padding={2}
-							sx={{
-								backgroundColor: Theme.palette.primary.main,
-							}}
-							alignItems="center"
-						>
-							<Avatar sx={{ marginTop: 4 }}>N</Avatar>
-							<AddIcon />
-						</Stack>
-						<Stack>
-							<Stack padding={2} paddingTop={5}>
-								<Typography variant="h6" component="h3">
-									Me & I
-								</Typography>
-								<Typography>Emanuel's Space</Typography>
-							</Stack>
-							{list()}
-						</Stack>
+		<Drawer
+			variant="permanent"
+			anchor={"left"}
+			sx={{
+				width: isExpanded ? EXPANDED_DRAWER_WIDTH : BASE_DRAWER_WIDTH,
+				height: "100vh",
+				flexShrink: 0,
+			}}
+		>
+			<Stack direction={"row"} height="100%">
+				<Stack
+					direction="column"
+					spacing={2}
+					padding={2}
+					sx={{
+						backgroundColor: Theme.palette.primary.main,
+					}}
+					alignItems="center"
+				>
+					<Button
+						color="success"
+						onClick={() => setIsExpanded((isExpanded) => !isExpanded)}
+					>
+						<AddIcon />
+					</Button>
+					<Avatar sx={{ marginTop: 4 }}>N</Avatar>
+					<AddIcon />
+				</Stack>
+				<Stack
+					sx={{
+						width: isExpanded ? EXPANDED_DRAWER_WIDTH - BASE_DRAWER_WIDTH : 0,
+						transition: "width 0.2s",
+					}}
+				>
+					<Stack padding={2} paddingTop={5}>
+						<Typography variant="h6" component="h3">
+							Me & I
+						</Typography>
+						<Typography>Emanuel's Space</Typography>
 					</Stack>
-				</Drawer>
-			</>
-		</div>
+					<SideBarItems />
+				</Stack>
+			</Stack>
+		</Drawer>
 	);
 }
+
+export default SideBar;
