5d7c3fa9da8a488a4dfe268da6bf38afabb36a3d
AleBarboni <alexa_wera@yahoo.com>
2023-04-20 19:06:38 +0300

    [108-fc7] adjusted modal and Task Description size

--- src/components/modal-content/CreateTask.jsx
+++ src/components/modal-content/CreateTask.jsx
@@ -37,90 +37,94 @@ function CreateTask() {
 	];
 
 	return (
-		<Stack>
-			<DialogActions>
-				<IconButton
-					onClick={() => dispatch(closeModal())}
-					color="theme.text.primary"
-				>
-					<CloseIcon />
-				</IconButton>
-			</DialogActions>
-			<DialogTitle variant="h4" fontWeight="bold">
-				Create Task
-			</DialogTitle>
-			<DialogContent>
-				<Stack gap={2}>
-					<Stack gap={1}>
-						<FormLabel>
-							<Typography fontWeight="medium">Task Name</Typography>
-						</FormLabel>
-						<TextField required variant="outlined" />
-					</Stack>
-					<Stack direction={"row"}>
-						<Grid container spacing={2}>
-							<Grid item xs={6}>
-								<Stack gap={1}>
-									<FormLabel>
-										<Typography fontWeight="medium">Task Priority</Typography>
-									</FormLabel>
-									<TextField select>
-										{options.map((option) => (
-											<MenuItem key={option.label} value={option.label}>
-												{option.label}
-											</MenuItem>
-										))}
-									</TextField>
-								</Stack>
+		<Stack sx={{ width: "35vw" }}>
+			<Stack sx={{ width: "100%" }}>
+				<DialogActions>
+					<IconButton
+						onClick={() => dispatch(closeModal())}
+						color="theme.text.primary"
+					>
+						<CloseIcon />
+					</IconButton>
+				</DialogActions>
+				<DialogTitle variant="h4" fontWeight="bold" sx={{ paddingY: 0 }}>
+					Create Task
+				</DialogTitle>
+				<DialogContent>
+					<Stack gap={2}>
+						<Stack gap={1}>
+							<FormLabel>
+								<Typography fontWeight="medium">Task Name</Typography>
+							</FormLabel>
+							<TextField required variant="outlined" />
+						</Stack>
+						<Stack direction={"row"}>
+							<Grid container spacing={2}>
+								<Grid item xs={6}>
+									<Stack gap={1}>
+										<FormLabel>
+											<Typography fontWeight="medium">Task Priority</Typography>
+										</FormLabel>
+										<TextField select>
+											{options.map((option) => (
+												<MenuItem key={option.label} value={option.label}>
+													{option.label}
+												</MenuItem>
+											))}
+										</TextField>
+									</Stack>
+								</Grid>
+								<Grid item xs={6}>
+									<Stack gap={1}>
+										<FormLabel>
+											<Typography fontWeight="medium">Due Date</Typography>
+										</FormLabel>
+										<TextField
+											onFocus={onFocus}
+											onBlur={onBlur}
+											InputProps={{
+												classes: {
+													input: "CustomTextField",
+												},
+												endAdornment: (
+													<InputAdornment position="end">
+														<CalendarMonthOutlinedIcon />
+													</InputAdornment>
+												),
+											}}
+											onChange={(e) => {
+												if (e.target.value) setHasValue(true);
+												else setHasValue(false);
+											}}
+											label="Today"
+											type={hasValue || focus ? "datetime-local" : "text"}
+											required
+											variant="outlined"
+										/>
+									</Stack>
+								</Grid>
 							</Grid>
-							<Grid item xs={6}>
-								<Stack gap={1}>
-									<FormLabel>
-										<Typography fontWeight="medium">Due Date</Typography>
-									</FormLabel>
-									<TextField
-										onFocus={onFocus}
-										onBlur={onBlur}
-										InputProps={{
-											classes: {
-												input: "CustomTextField",
-											},
-											endAdornment: (
-												<InputAdornment position="end">
-													<CalendarMonthOutlinedIcon />
-												</InputAdornment>
-											),
-										}}
-										onChange={(e) => {
-											if (e.target.value) setHasValue(true);
-											else setHasValue(false);
-										}}
-										label="Today"
-										type={hasValue || focus ? "datetime-local" : "text"}
-										required
-										variant="outlined"
-									/>
-								</Stack>
-							</Grid>
-						</Grid>
+						</Stack>
+						<Stack gap={1}>
+							<FormLabel>
+								<Typography fontWeight="medium">Task Description</Typography>
+							</FormLabel>
+							<TextField
+								required
+								variant="outlined"
+								placeholder="Type your content here...."
+								multiline
+								rows={3}
+							/>
+						</Stack>
+						<Stack justifyContent="flex-start" alignItems="flex-start">
+							<Button variant="contained" color="primary" size="large">
+								Create Task
+							</Button>
+						</Stack>
 					</Stack>
-					<Stack gap={1}>
-						<FormLabel>
-							<Typography fontWeight="medium">Task Description</Typography>
-						</FormLabel>
-						<TextField
-							required
-							variant="outlined"
-							placeholder="Type your content here...."
-						/>
-					</Stack>
-					<Stack justifyContent="flex-start" alignItems="flex-start">
-						<Button variant="contained" color="primary" size="large">
-							Create Task
-						</Button>
-					</Stack>
-				</Stack>
-			</DialogContent>
+				</DialogContent>
+			</Stack>
 		</Stack>
 	);
 }
