eea3c81a61f10aa1a83c7125d762275d85214820
HalaszPaul <115172462+HalaszPaul@users.noreply.github.com>
2023-04-23 11:35:27 +0300

    674-aab created getTasks function

--- src/services/tasks-service.js
+++ src/services/tasks-service.js
@@ -1,1 +1,13 @@
 import { ENDPOINTS } from "../constants/api";
+
+
+const getTasks = async (workspaceId) => {
+    const token = localStorage.getItem("token");
+
+    const response = await fetch(`${ENDPOINTS.TASKS}/v1/tasks?workspaceId=${workspaceId}`, {
+        headers: {
+            "Authorization": `Bearer ${token}`,
+        },
+    });
+    
+}
