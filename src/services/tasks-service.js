43dbb9d0830111e1c5821a8d9cb7292b03f0ab1f
HalaszPaul <115172462+HalaszPaul@users.noreply.github.com>
2023-04-23 12:51:59 +0300

    674 - aab modified task service

--- src/services/tasks-service.js
+++ src/services/tasks-service.js
@@ -3,18 +3,24 @@ import { ENDPOINTS } from "../constants/api";
 
 const getTasks = async (workspaceId) => {
     const token = localStorage.getItem("token");
-
-    const response = await fetch(`${ENDPOINTS.TASKS}/v1/tasks?workspaceId=${workspaceId}`, {
-        headers: {
-            "Authorization": `Bearer ${token}`,
-        },
-    });
-    if (response.status !== 200) {
+    if(!token) {
         return [];
     }
-    const data = await response.json();
-    return data.items;
+    const response = await fetch(ENDPOINTS.TASKS.GET_TASKS(workspaceId), {
+        headers: {
+            "Authorization": `Bearer ${token}`
+    },
+});
+
+if (response.status !== 200) {
+    return[];
+}
+const data = await response.json();
+return data.items;
 };
+
+      
+   
 const taskService = {
     getTasks,
 };
