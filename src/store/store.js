c60912c90a91defd2a14c92cf0bf6859ed519941
HalaszPaul <115172462+HalaszPaul@users.noreply.github.com>
2023-04-23 13:22:42 +0300

    674-aab imported taskreducer in store

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import CounterReducer from "./slices/counter-slice";
 import AuthenticationReducer from "./slices/authentication-slice";
 import ModalReducer from "./slices/modal-slice";
+import TaskReducer from "./slices/tasks-slice";
 
 const store = configureStore({
 	reducer: {
 		counter: CounterReducer,
 		authentication: AuthenticationReducer,
 		modal: ModalReducer,
+		tasks: TaskReducer,
 	},
 });
 
