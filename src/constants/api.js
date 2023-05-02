c2335389dcd37882eb00ece98d8cb7d90224865c
Groza Zita <adi_groza@yahoo.com>
2023-05-02 17:25:06 +0300

    [644-7e5] created workspace service

--- src/constants/api.js
+++ src/constants/api.js
@@ -4,4 +4,7 @@ export const ENDPOINTS = {
 	AUTH: {
 		LOGIN: `${API}/v1/auth/login`,
 	},
+	WORKSPACE:{
+        USER_WORKSPACE: `${API}/v1/workspaces`,
+    }
 };
---
+++ src/services/workspace-service.js
@@ -0,0 +1,23 @@
+import { ENDPOINTS } from "../constants/api";
+import { setWorkspace } from "../store/slices/workspace-slice";
+
+const getWorkspace = async () => {
+	const token = localStorage.getItem("token");
+
+	const response = await fetch(ENDPOINTS.AUTH.WORKSPACE.USER_WORKSPACE, {
+		method: "GET",
+		headers: {
+			Authorization: `Bearer ${token}}`,
+		},
+	});
+		
+    if (response.status === 200) {
+        const data = await response.json();
+        localStorage.setWorkSpace("token", data.token);
+        dispatch(setWorkspace(data));
+      } else {
+        dispatch(setWorkspace([]));
+    }
+};
+
+export default getWorkspace;
