69a0d887a69fbf30c1a5bacf4fa9f2fb8033f943
cezarmocanu <cezarmocanu2014@gmail.com>
2023-05-04 20:00:27 +0300

    [114-038] Fixed mdoal styling

--- src/components/modal-content/DeleteModalContent.jsx
+++ src/components/modal-content/DeleteModalContent.jsx
@@ -1,43 +1,53 @@
 import React from "react";
-import Stack from '@mui/material/Stack';
-import CloseIcon from '@mui/icons-material/Close';
-import Typography from '@mui/material/Typography';
-import Theme from "../../theme";
+import Stack from "@mui/material/Stack";
+import CloseIcon from "@mui/icons-material/Close";
 import { useDispatch } from "react-redux";
 import { closeModal } from "../../store/slices/modal-slice";
-import {
-    DialogTitle,
-    DialogContent,
-    DialogContentText,
-    DialogActions,
-    Button,
-} from "@mui/material";
+import { DialogTitle, DialogContent, Button, Typography } from "@mui/material";
 
 function DeleteModalContent() {
-    const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-    return (
-        <>
-        <Stack direction="row" sx={{width: '100%',
-    justifyContent: "space-between"}}>
-
-            <DialogTitle variant="h2">Delete task</DialogTitle>
-        <CloseIcon onClick={() => dispatch(closeModal())}></CloseIcon>
-        </Stack>
-
-            <DialogContent>
-                <DialogContentText>
-                    Are you sure you delete the task? This task is in-progress?
-                </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-                <Button color="primary" variant="contained" size="large"onClick={() => dispatch(closeModal())}>No</Button>
-
-                <Button sx={{
-                        backgroundColor: Theme.palette.info.light,
-                    }} color="error" variant="contained" size="large" onClick={() => dispatch(closeModal())}>Yes</Button>
-            </DialogActions>
-        </>
-    );
+	return (
+		<>
+			<DialogTitle>
+				<Stack
+					direction="row"
+					sx={{
+						width: "100%",
+						justifyContent: "space-between",
+					}}
+				>
+					<Typography variant="h4">Delete task</Typography>
+					<CloseIcon onClick={() => dispatch(closeModal())} />
+				</Stack>
+			</DialogTitle>
+			<DialogContent>
+				<Stack gap={2}>
+					<Typography variant="body1">
+						Are you sure you delete the task? This task is in-progress?
+					</Typography>
+					<Stack direction={"horizontal"} gap={1}>
+						<Button
+							color="primary"
+							variant="contained"
+							size="large"
+							onClick={() => dispatch(closeModal())}
+						>
+							No
+						</Button>
+						<Button
+							color="error"
+							variant="contained"
+							size="large"
+							onClick={() => dispatch(closeModal())}
+						>
+							Yes
+						</Button>
+					</Stack>
+				</Stack>
+			</DialogContent>
+		</>
+	);
 }
 export default DeleteModalContent;
