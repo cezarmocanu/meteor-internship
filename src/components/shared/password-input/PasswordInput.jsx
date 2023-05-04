6848986cdcf57bb816b2a684d405106c2d8f00f4
unknown <nedelescuramona@yahoo.com>
2023-05-04 20:36:16 +0300

    debugg

---
+++ src/components/shared/password-input/PasswordInput.jsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import { useState } from "react";
+import { useTheme } from "@mui/material/styles";
+import { Stack, InputAdornment, IconButton } from "@mui/material";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
+import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
+import TextField from "../../shared/input/TextField";
+import FormLabel from "../input/FormLabel";
+import TextField from "../input/TextField";
+
+function PasswordInput() {
+	const [showPassword, setShowPassword] = useState(false);
+	const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+	return (
+		<Stack alignItems="left">
+			<Stack gap={1}>
+				<TextField
+					type={showPassword ? "text" : "password"}
+					InputProps={{
+						endAdornment: (
+							<InputAdornment position="end">
+								<IconButton
+									aria-label="toggle password visibility"
+									onClick={handleClickShowPassword}
+								>
+									{showPassword ? (
+										<VisibilityOffOutlinedIcon />
+									) : (
+										<VisibilityOutlinedIcon />
+									)}
+								</IconButton>
+							</InputAdornment>
+						),
+					}}
+					variant="outlined"
+					size="medium"
+				/>
+			</Stack>
+		</Stack>
+	);
+}
+
+export default PasswordInput;
