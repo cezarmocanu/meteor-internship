671b3f93c116dbafa098323278d30a9ae19868f8
Stan Marius <stanmarius2001@gmail.com>
2023-05-16 18:44:32 +0300

    LoginForm formik

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,150 +1,71 @@
-import React, { useState } from "react";
-import { Typography, Stack, Box } from "@mui/material";
-import { useTheme } from "@mui/material/styles";
-import { useNavigate } from "react-router-dom";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-
-import ContainedImage from "../components/ContainedImage";
-import Button from "../components/shared/button/Button";
-import FormLabel from "../components/shared/input/FormLabel";
-import TextField from "../components/shared/input/TextField";
-import authService from "../services/auth-service";
-import RoutePaths from "../constants/route-paths";
-import PasswordInput from "../components/shared/password-input/PasswordInput";
-import { useDispatch } from "react-redux";
-import { login } from "../store/slices/authentication-slice";
+import React from "react";
+import { useFormik } from "formik";
+import { FormLabel, TextField, Button, Stack } from "@mui/material";
+import * as yup from "yup";
+import PasswordInput from "../shared/password-input/PasswordInput";
 
 function LoginPage() {
-	const theme = useTheme();
-	const navigate = useNavigate();
-	const dispatch = useDispatch();
+	const onSubmit = (values) => {
+		console.log("from button", values);
+	};
 
-	const [loginData, setLoginData] = useState({
-		username: "",
-		password: "",
+	const formik = useFormik({
+		initialValues: INITIAL_VALUES,
+		validationSchema,
+		onSubmit,
 	});
 
-	const onTextFieldChange = (e) => {
-		setLoginData((prevState) => ({
-			...prevState,
-			[e.target.name]: e.target.value,
-		}));
-	};
-
-	const handleLoginClick = () => {
-		authService
-			.login(loginData.username, loginData.password)
-			.then((loggedInWithSuccess) => {
-				if (!loggedInWithSuccess) {
-					return;
-				}
-
-				dispatch(login());
-				navigate(RoutePaths.TEST);
-			});
-	};
-
 	return (
-		<Stack sx={{ width: "100%", height: "100vh" }}>
-			<Stack sx={{ height: "100%" }} direction={"row"}>
-				<Stack flexGrow={1} justifyContent="center" alignItems="center">
-					<Stack
-						gap={8}
-						sx={{
-							[theme.breakpoints.up("lg")]: {
-								width: "60%",
-							},
-							[theme.breakpoints.up("md")]: {
-								width: "80%",
-							},
-							width: "90%",
-						}}
-					>
-						<Stack>
-							<Typography variant="h5" fontWeight="bold">
-								Welcome Back
-							</Typography>
-						</Stack>
-						<Stack>
-							<Stack>
-								<FormLabel>Email Address</FormLabel>
-								<TextField
-									name="username"
-									value={loginData.username}
-									onChange={onTextFieldChange}
-									required
-									variant="outlined"
-									helperText="Example. mano@gmail.com"
-								/>
-							</Stack>
-							<Stack>
-								<FormLabel>Enter your Password</FormLabel>
-								<PasswordInput
-									name="password"
-									value={loginData.password}
-									onChange={onTextFieldChange}
-								/>
-							</Stack>
-							<FormControlLabel
-								control={<Checkbox checked={false} />}
-								label={<Typography fontWeight="bold"> Remember me</Typography>}
-							/>
-						</Stack>
-
-						<Stack gap={1}>
-							<Button
-								variant="contained"
-								color="primary"
-								size="large"
-								onClick={handleLoginClick}
-							>
-								Log in
-							</Button>
-
-							<Typography color="primary" fontWeight="bold">
-								Forgot Password?
-							</Typography>
-							<Typography color="primary" fontWeight="bold">
-								Don't have an account? Create one <a href="/signup">here</a>
-							</Typography>
-						</Stack>
-					</Stack>
-				</Stack>
-				<Stack
-					sx={{
-						height: "100%",
-						width: "auto",
-						position: "relative",
-					}}
-				>
-					<ContainedImage
-						width="100%"
-						height="100%"
-						sx={{ transform: "rotateY(180deg)" }}
-						src="logo-background.png"
-					/>
-					<Stack
-						justifyContent="space-between"
-						alignItems="flex-end"
-						sx={{ position: "absolute", width: "100%", height: "100%", p: 6 }}
-					>
-						<Box />
-						<Typography
-							variant="h3"
-							fontWeight="bold"
-							color="white"
-							sx={{ width: "80%" }}
-						>
-							Take your productivity to the next level.
-						</Typography>
-						<Typography variant="body2" color="white">
-							SkillBrain 2022 | All Right Reserved
-						</Typography>
-					</Stack>
-				</Stack>
+		<>
+			<Stack gap={1}>
+				<FormLabel>Email Address</FormLabel>
+				<TextField
+					name="email"
+					onChange={formik.handleChange}
+					value={formik.values.email}
+					error={formik.touched.email && Boolean(formik.errors.email)}
+					helperText={formik.touched.email && formik.errors.email}
+					variant="outlined"
+					size="small"
+				/>
+				<FormLabel>Password</FormLabel>
+				<PasswordInput
+					name="password"
+					onChange={formik.handleChange}
+					value={formik.values.password}
+					error={formik.touched.password && Boolean(formik.errors.password)}
+					helperText={formik.touched.password && formik.errors.password}
+					size="small"
+				/>
 			</Stack>
-		</Stack>
+
+			<Button
+				onClick={formik.handleSubmit}
+				type="submit"
+				variant="contained"
+				color="primary"
+				size="large"
+			>
+				Log In
+			</Button>
+		</>
 	);
 }
+
+const INITIAL_VALUES = {
+	email: "",
+	password: "",
+};
+
+const validationSchema = yup.object({
+	email: yup
+		.string("Enter your email")
+		.email("Enter a valid email")
+		.required("Email is required"),
+	password: yup
+		.string("Enter your password")
+		.min(8, "Password should be of minimum 8 characters length")
+		.required("Password is required"),
+});
+
 export default LoginPage;
